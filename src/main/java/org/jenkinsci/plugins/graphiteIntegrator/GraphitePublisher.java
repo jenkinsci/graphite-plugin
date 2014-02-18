799d44f7fadac4f54cbd1cfa8c62cc19b588c033
Josh Sinfield <joshua.sinfield@cdl.co.uk>
2014-02-18 17:52:57 +0000

    Add Build_failed and Build_Successful metrics
    also added correct classpath and project file
    
    Change-Id: I499bffe890d28668e9da79b21d4260a7f7a80244

--- src/main/java/org/jenkinsci/plugins/graphiteIntegrator/GraphitePublisher.java
+++ src/main/java/org/jenkinsci/plugins/graphiteIntegrator/GraphitePublisher.java
@@ -18,8 +18,8 @@ import java.util.List;
 
 import org.jenkinsci.plugins.graphiteIntegrator.metrics.AbstractMetric;
 import org.jenkinsci.plugins.graphiteIntegrator.metrics.BuildDurationMetric;
-
-
+import org.jenkinsci.plugins.graphiteIntegrator.metrics.BuildFailedMetric;
+import org.jenkinsci.plugins.graphiteIntegrator.metrics.BuildSuccessfulMetric;
 import org.jenkinsci.plugins.graphiteIntegrator.metrics.CoberturaCodeCoverageMetric;
 import org.jenkinsci.plugins.graphiteIntegrator.metrics.FailTestsMetric;
 import org.jenkinsci.plugins.graphiteIntegrator.metrics.SkipTestsMetric;
@@ -33,246 +33,244 @@ import utils.MetricsEnum;
  */
 public class GraphitePublisher extends Notifier {
 
-        /**
+    /**
      *
      */
-        @Extension
-        public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+    @Extension
+    public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
 
-        /**
+    /**
      *
      */
-        private String selectedIp;
+    private String selectedIp;
 
-        /**
+    /**
      *
      */
-        private String selectedMetric;
+    private String selectedMetric;
 
-        /**
+    /**
      *
      */
+    private String protocol;
 
+    private List<Metric> metrics = new ArrayList<Metric>();
 
-
-
-
-        private List<Metric> metrics = new ArrayList<Metric>();
-
-        /**
+    /**
      *
      */
-        public GraphitePublisher() {
+    public GraphitePublisher() {
+    }
+
+    /**
+     *
+     */
+    public GraphitePublisher(String ip, String metric, String protocol) {
+        this.selectedIp = ip;
+        this.selectedMetric = metric;
+        this.protocol = protocol;
+        System.out.println("IP: " + ip);
+        System.out.println("Protocol: " + protocol);
+
+    }
+
+    /**
+     *
+     */
+    public List<Metric> getMetrics() {
+        return metrics;
+    }
+
+    /**
+     *
+     */
+    public String getSelectedIp() {
+        String ipTemp = selectedIp;
+        if (ipTemp == null) {
+            Server[] servers = DESCRIPTOR.getServers();
+            if (servers.length > 0) {
+                ipTemp = servers[0].getIp();
+            }
+        }
+        return ipTemp;
+    }
+
+    /**
+     *
+     */
+    public String getSelectedMetric() {
+        String metricTemp = selectedMetric;
+        if (metricTemp == null) {
+            Metric[] metrics = DESCRIPTOR.getMetrics();
+            if (metrics.length > 0) {
+                metricTemp = metrics[0].getName();
+            }
+        }
+        return metricTemp;
+    }
+
+    /**
+     *
+     */
+    public void setSelectedIp(String ip) {
+        this.selectedIp = ip;
+    }
+
+    /**
+     *
+     */
+    public void setSelectedMetric(String metric) {
+        this.selectedMetric = metric;
+    }
+
+    /**
+     *
+     */
+    public Server getServer() {
+        Server[] servers = DESCRIPTOR.getServers();
+        if (selectedIp == null && servers.length > 0) {
+            return servers[0];
+        }
+        for (Server server : servers) {
+            if (server.getIp().equals(selectedIp)) {
+                return server;
+            }
+        }
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * hudson.tasks.BuildStepCompatibilityLayer#prebuild(hudson.model.AbstractBuild
+     * , hudson.model.BuildListener)
+     */
+    @Override
+    public boolean prebuild(AbstractBuild<?, ?> build, BuildListener listener) {
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see hudson.tasks.Publisher#needsToRunAfterFinalized()
+     */
+    @Override
+    public boolean needsToRunAfterFinalized() {
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see hudson.tasks.BuildStep#getRequiredMonitorService()
+     */
+    @Override
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.NONE;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see hudson.tasks.Notifier#getDescriptor()
+     */
+    @Override
+    public BuildStepDescriptor<Publisher> getDescriptor() {
+        return DESCRIPTOR;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * hudson.tasks.BuildStepCompatibilityLayer#perform(hudson.model.AbstractBuild
+     * , hudson.Launcher, hudson.model.BuildListener)
+     */
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+
+        if (build.getResult() == Result.ABORTED) {
+            return true;
         }
 
-        /**
-     *
-     */
-        public GraphitePublisher(String ip, String metric, String protocol) {
-                this.selectedIp = ip;
-                this.selectedMetric = metric;
-                this.protocol = protocol;
-                System.out.println("IP: " + ip);
-                System.out.println("Protocol: " + protocol);
-
+        if (getServer() == null) {
+            return false;
         }
 
-        /**
-     *
-     */
-        public List<Metric> getMetrics() {
-                return metrics;
-        }
+        listener.getLogger().println("Connecting to " + getServer().getDescription());
 
-        /**
-     *
-     */
-        public String getSelectedIp() {
-                String ipTemp = selectedIp;
-                if (ipTemp == null) {
-                        Server[] servers = DESCRIPTOR.getServers();
-                        if (servers.length > 0) {
-                                ipTemp = servers[0].getIp();
-                        }
+        GraphiteLogger graphiteLogger = new GraphiteLogger(listener.getLogger());
+
+        AbstractMetric metricSender = null;
+
+        List<Metric> coberturaMetrics = null;
+
+        for (Metric metric : metrics) {
+            if (metric.name.equals(MetricsEnum.BUILD_DURATION.name())) {
+                metricSender = new BuildDurationMetric(build, listener.getLogger(), graphiteLogger, DESCRIPTOR.getBaseQueueName());
+                metricSender.sendMetric(getServer(), metric);
+            }
+            if (metric.name.equals(MetricsEnum.BUILD_FAILED.name())) {
+                metricSender = new BuildFailedMetric(build, listener.getLogger(), graphiteLogger, DESCRIPTOR.getBaseQueueName());
+                metricSender.sendMetric(getServer(), metric);
+            }
+            if (metric.name.equals(MetricsEnum.BUILD_SUCCESSFUL.name())) {
+                metricSender = new BuildSuccessfulMetric(build, listener.getLogger(), graphiteLogger, DESCRIPTOR.getBaseQueueName());
+                metricSender.sendMetric(getServer(), metric);
+            }
+            if (isCoberturaMetric(metric)) {
+                if (!isCoberturaListInitialized(coberturaMetrics)) {
+                    coberturaMetrics = new ArrayList<Metric>();
                 }
-                return ipTemp;
+                coberturaMetrics.add(metric);
+            }
+            // If a Freestyle Build has been configured (without publishing
+            // JUnit XML Results) these will fail.
+            // Added simple null check in for now to be safe.
+            if (build.getTestResultAction() != null) {
+                if (metric.name.equals(MetricsEnum.FAIL_TESTS.name())) {
+                    metricSender = new FailTestsMetric(build, listener.getLogger(), graphiteLogger, DESCRIPTOR.getBaseQueueName());
+                    metricSender.sendMetric(getServer(), metric);
+                }
+                if (metric.name.equals(MetricsEnum.SKIPED_TESTS.name())) {
+                    metricSender = new SkipTestsMetric(build, listener.getLogger(), graphiteLogger, DESCRIPTOR.getBaseQueueName());
+                    metricSender.sendMetric(getServer(), metric);
+                }
+                if (metric.name.equals(MetricsEnum.TOTAL_TESTS.name())) {
+                    metricSender = new TotalTestsMetric(build, listener.getLogger(), graphiteLogger, DESCRIPTOR.getBaseQueueName());
+                    metricSender.sendMetric(getServer(), metric);
+                }
+            }
+        }
+        if (isCoberturaListInitialized(coberturaMetrics)) {
+            metricSender = new CoberturaCodeCoverageMetric(build, listener.getLogger(), graphiteLogger, DESCRIPTOR.getBaseQueueName());
+            metricSender.sendMetric(getServer(), coberturaMetrics.toArray(new Metric[coberturaMetrics.size()]));
         }
 
-        /**
-     *
+        return true;
+    }
+
+    /**
+     * @param coberturaMetrics
+     * @return
      */
-        public String getSelectedMetric() {
-                String metricTemp = selectedMetric;
-                if (metricTemp == null) {
-                        Metric[] metrics = DESCRIPTOR.getMetrics();
-                        if (metrics.length > 0) {
-                                metricTemp = metrics[0].getName();
-                        }
-                }
-                return metricTemp;
-        }
+    private boolean isCoberturaListInitialized(List<Metric> coberturaMetrics) {
+        return coberturaMetrics != null;
+    }
 
-        /**
-     *
+    /**
+     * @param metric
+     * @return
      */
-        public void setSelectedIp(String ip) {
-                this.selectedIp = ip;
-        }
+    private boolean isCoberturaMetric(Metric metric) {
+        return (// metric.name.equals(MetricsEnum.COBERTURA_PACKAGE_BRANCH_COVERAGE.name())
+                // ||
+                // metric.name.equals(MetricsEnum.COBERTURA_PACKAGE_LINE_COVERAGE.name())
+                // ||
+        metric.name.equals(MetricsEnum.COBERTURA_TOTAL_BRANCH_COVERAGE.name()) || metric.name.equals(MetricsEnum.COBERTURA_TOTAL_LINE_COVERAGE.name())
 
-        /**
-     *
-     */
-        public void setSelectedMetric(String metric) {
-                this.selectedMetric = metric;
-        }
-
-        /**
-     *
-     */
-        public Server getServer() {
-                Server[] servers = DESCRIPTOR.getServers();
-                if (selectedIp == null && servers.length > 0) {
-                        return servers[0];
-                }
-                for (Server server : servers) {
-                        if (server.getIp().equals(selectedIp)) {
-                                return server;
-                        }
-                }
-                return null;
-        }
-
-        /*
-         * (non-Javadoc)
-         * 
-         * @see hudson.tasks.BuildStepCompatibilityLayer#prebuild(hudson.model.AbstractBuild, hudson.model.BuildListener)
-         */
-        @Override
-        public boolean prebuild(AbstractBuild<?, ?> build, BuildListener listener) {
-                return true;
-        }
-
-        /*
-         * (non-Javadoc)
-         * 
-         * @see hudson.tasks.Publisher#needsToRunAfterFinalized()
-         */
-        @Override
-        public boolean needsToRunAfterFinalized() {
-                return true;
-        }
-
-        /*
-         * (non-Javadoc)
-         * 
-         * @see hudson.tasks.BuildStep#getRequiredMonitorService()
-         */
-        @Override
-        public BuildStepMonitor getRequiredMonitorService() {
-                return BuildStepMonitor.NONE;
-        }
-
-        /*
-         * (non-Javadoc)
-         * 
-         * @see hudson.tasks.Notifier#getDescriptor()
-         */
-        @Override
-        public BuildStepDescriptor<Publisher> getDescriptor() {
-                return DESCRIPTOR;
-        }
-
-        /*
-         * (non-Javadoc)
-         * 
-         * @see hudson.tasks.BuildStepCompatibilityLayer#perform(hudson.model.AbstractBuild, hudson.Launcher,
-         * hudson.model.BuildListener)
-         */
-        @Override
-        public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
-                        throws InterruptedException, IOException {
-
-
-
-                if (build.getResult() == Result.ABORTED) {
-                        return true;
-                }
-
-
-                if (getServer() == null) {
-                        return false;
-                }
-
-                listener.getLogger().println("Connecting to " + getServer().getDescription());
-
-                GraphiteLogger graphiteLogger = new GraphiteLogger(listener.getLogger());
-
-                AbstractMetric metricSender = null;
-
-                List<Metric> coberturaMetrics = null;
-
-                for (Metric metric : metrics) {
-                        if (metric.name.equals(MetricsEnum.BUILD_DURATION.name())) {
-                                metricSender = new BuildDurationMetric(build, listener.getLogger(), graphiteLogger, DESCRIPTOR.getBaseQueueName());
-                                metricSender.sendMetric(getServer(), metric);
-                        }
-                        if(metric.name.equals(MetricsEnum.BUILD_FAILED.name())){
-                                metricSender = new BuildFailedMetric(build, listener.getLogger(), graphiteLogger, DESCRIPTOR.getBaseQueueName());
-                                metricSender.sendMetric(getServer(), metric);
-                        }
-                        if(metric.name.equals(MetricsEnum.BUILD_SUCCESSFUL.name())){
-                                metricSender = new BuildSuccessfulMetric(build, listener.getLogger(), graphiteLogger, DESCRIPTOR.getBaseQueueName());
-                                metricSender.sendMetric(getServer(), metric);
-                        }
-                        if (isCoberturaMetric(metric)) {
-                                if (!isCoberturaListInitialized(coberturaMetrics)) {
-                                        coberturaMetrics = new ArrayList<Metric>();
-                                }
-                                coberturaMetrics.add(metric);
-                        }
-						// If a Freestyle Build has been configured (without saving JUnit XML Results, I think) these will fail.
-						// Added simple null check in for now to be safe.
-						if (build.getTestResultAction() != null) {
-							if (metric.name.equals(MetricsEnum.FAIL_TESTS.name())) {
-									metricSender = new FailTestsMetric(build, listener.getLogger(), graphiteLogger, DESCRIPTOR.getBaseQueueName());
-									metricSender.sendMetric(getServer(), metric);
-							}
-							if (metric.name.equals(MetricsEnum.SKIPED_TESTS.name())) {
-									metricSender = new SkipTestsMetric(build, listener.getLogger(), graphiteLogger, DESCRIPTOR.getBaseQueueName());
-									metricSender.sendMetric(getServer(), metric);
-							}
-							if (metric.name.equals(MetricsEnum.TOTAL_TESTS.name())) {
-									metricSender = new TotalTestsMetric(build, listener.getLogger(), graphiteLogger, DESCRIPTOR.getBaseQueueName());
-									metricSender.sendMetric(getServer(), metric);
-							}
-						}
-                }
-                if (isCoberturaListInitialized(coberturaMetrics)) {
-                        metricSender = new CoberturaCodeCoverageMetric(build, listener.getLogger(), graphiteLogger, DESCRIPTOR.getBaseQueueName());
-                        metricSender.sendMetric(getServer(), coberturaMetrics.toArray(new Metric[coberturaMetrics.size()]));
-                }
-
-                return true;
-        }
-
-        /**
-         * @param coberturaMetrics
-         * @return
-         */
-        private boolean isCoberturaListInitialized(List<Metric> coberturaMetrics) {
-                return coberturaMetrics != null;
-        }
-
-        /**
-         * @param metric
-         * @return
-         */
-        private boolean isCoberturaMetric(Metric metric) {
-                return (// metric.name.equals(MetricsEnum.COBERTURA_PACKAGE_BRANCH_COVERAGE.name())
-                                // || metric.name.equals(MetricsEnum.COBERTURA_PACKAGE_LINE_COVERAGE.name())
-                                // ||
-                metric.name.equals(MetricsEnum.COBERTURA_TOTAL_BRANCH_COVERAGE.name()) || metric.name.equals(MetricsEnum.COBERTURA_TOTAL_LINE_COVERAGE.name())
-
-                );
-        }
+        );
+    }
 }
